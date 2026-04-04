e46736e1e1c6e58c71d0d738c244fa5d862bdee4
rafael-centanin <rcentaninmoline@udesa.edu.ar>
2026-04-04 12:28:28 -0300

    Avance con las descripciones de las peliculas y la funcionalidad de los botones

--- src/componentes/MasPopulares/MasPopulares.js
+++ src/componentes/MasPopulares/MasPopulares.js
@@ -1,26 +1,54 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
 import "./MasPopulares.css"
-function MasPopulares(props) {
-    return (
-        <div className="divMasPopular">
-                {props.peliculas.map((pelicula) =>(
-                    <article className="peliculaMasPopular">
-                        <img
-                            src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}.jpg`}
-                            alt={pelicula.title}
-                        />
-                        <p>{pelicula.title}</p>
-                        <p>{pelicula.overview}</p>
-                        <button> Ver Descripcion</button>
-                        <button> Ir a detalle</button>
-                        <button> Agregar/ quitar de favoritos</button>
 
 
-                    </article>
-                ))}
+class MasPopulares extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            show : false,
+            hide : true
 
+        }
+    }
+
+    show() {
+        this.setState({
+            show : this.state.show=true
+        })
+    }
+
+    hide() {
+        this.setState({
+            show: this.state.show=false,
+        })
+    }
+
+    render(){
+        return (
+
+        <div className="divEnCartel">
+            {this.props.peliculas.map((pelicula) =>(
+            <article className="peliculaEnCartel">
+            <img
+                src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}.jpg`}
+                    alt={pelicula.title}
+                    />
+            <h2 > <Link to= "">{pelicula.title} </Link></h2>
+
+            {this.state.show === true ? <p>{pelicula.overview}</p>  : null }
+            {this.state.show=== true ?  <button className='more' onClick={()=> this.hide()}>Ver Menos</button>: 
+            <button className='more' onClick={()=> this.show()}> Ver Descripcion</button>}
+
+            
+		    <button className='delete'onClick={() => ""()}>Ir a detalle</button>
+            <button className='fav'onClick={() => this.props.favoritos()}>Ad favoritos</button> 
+        </article>
+        ))}
         </div>
-    )
+        )
+    }
 }
 
 
--- src/componentes/PelisCartel/PelisCartel.js
+++ src/componentes/PelisCartel/PelisCartel.js
@@ -1,26 +1,51 @@
-import React from "react";
+
 import "./PelisCartel.css"
-function PelisCartel(props) {
-    return (
+import { Link } from "react-router-dom";
+import React, { Component } from "react";
+
+
+class PelisCartel extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            show : false,
+            hide : true
+
+        }
+    }
+
+    show() {
+        this.setState({
+            show : this.state.show=true
+        })
+    }
+
+    hide() {
+        this.setState({
+            show: this.state.show=false,
+        })
+    }
+
+    render(){
+        return (
+
         <div className="divEnCartel">
-                {props.peliculas.map((pelicula) =>(
-                    <article className="peliculaEnCartel">
-                        <img
-                            src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}.jpg`}
-                            alt={pelicula.title}
-                        />
-                        <p>{pelicula.title}</p>
-                        <p>{pelicula.overview}</p>
-                        <button> Ver Descripcion</button>
-                        <button> Ir a detalle</button>
-                        <button> Agregar/ quitar de favoritos</button>
+            {this.props.peliculas.map((pelicula) =>(
+            <article className="peliculaEnCartel">
+            <img src={`https://image.tmdb.org/t/p/w342/${pelicula.poster_path}.jpg`} alt={pelicula.title} />
+            <h2 > <Link to= "">{pelicula.title} </Link></h2>
 
-                    </article>
-                ))}
+            {this.state.show === true ? <p>{pelicula.overview}</p>  : null }
+            {this.state.show=== true ?  <button className='more' onClick={()=> this.hide()}>Ver Menos</button>: 
+            <button className='more' onClick={()=> this.show()}> Ver Descripcion</button>}
 
+            
+		    <button className='delete'onClick={() => ""()}>Ir a detalle</button>
+            <button className='fav'onClick={() => this.props.favoritos()}>Ad favoritos</button> 
+        </article>
+        ))}
         </div>
-    )
+        )
+    }
 }
-
-
 export default PelisCartel;
