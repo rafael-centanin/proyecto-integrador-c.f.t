ccbfcc79b4413af6b9f73789460fa1a8fcc83664
abrilfari <afarinaccia@udesa.edu.ar>
2026-04-18 17:38:11 -0300

    Cambio en la ruta del register y algunos detalles

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -18,7 +18,7 @@ class Favoritos extends Component {
             let favoritos = JSON.parse(storage);
             let peliculasRecuperadas = [];
             favoritos.map((id) => {
-                fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0185f70c5f71076c61606afd4f75803b`)
+                 return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0185f70c5f71076c61606afd4f75803b`)
                     .then(res => res.json())
                     .then(data => {
                         peliculasRecuperadas.push(data);
@@ -40,7 +40,7 @@ class Favoritos extends Component {
             let favoritos = JSON.parse(storage);
             let peliculasRecuperadas = [];
             favoritos.map((id) => {
-                fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=0185f70c5f71076c61606afd4f75803b`)
+                return fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=0185f70c5f71076c61606afd4f75803b`)
                     .then(res => res.json())
                     .then(data => {
                         peliculasRecuperadas.push(data);
@@ -60,37 +60,36 @@ class Favoritos extends Component {
         console.log(localStorage)
         return (
             <div className="fondoMovie">
-            <main className="mainHome">
-                <section>
-                    <h2 id="h2Main" class="nav-link"> Películas Favoritas</h2>
-                    {this.state.peliculasFav.length === 0 ? <p className="PFavoritos"  id="copy">No tenés peliculas favoritas</p> :
-                        <div className="divEnCartel">
-                            {this.state.peliculasFav.map((pelicula, idx) => (
-                                <CardPeli key={idx + 1}
-                                    img={pelicula.poster_path}
-                                    title={pelicula.title}
-                                    id={pelicula.id}
-                                    overview={pelicula.overview}
-                                    type="movie" />
-                            ))}
-                        </div>}
-                </section>
-                <section>
-                    <h2 id="h2Main" className="nav-link"> Series Favoritas</h2>
-                    {this.state.dataSeries.length === 0 ? <p className="PFavoritos"id="copy">No tenés series favoritas</p> :
-                        <div className="divEnCartel">
-                            {this.state.dataSeries.map((pelicula, idx) => (
-                                <CardSeries key={idx + 1}
-                                    img={pelicula.poster_path}
-                                    title={pelicula.name}
-                                    id={pelicula.id}
-                                    overview={pelicula.overview}
-                                    type="tv" />
-                            ))}
-                            <button>Cargar mas</button>
-                        </div>}
-                </section>
-            </main>
+                <main className="mainHome">
+                    <section>
+                        <h2 id="h2Main" className="nav-link"> Películas Favoritas</h2>
+                        {this.state.peliculasFav.length === 0 ? <p className="PFavoritos" id="copy">No tenés peliculas favoritas</p> :
+                            <div className="divEnCartel">
+                                {this.state.peliculasFav.map((pelicula, idx) => (
+                                    <CardPeli key={idx + 1}
+                                        img={pelicula.poster_path}
+                                        title={pelicula.title}
+                                        id={pelicula.id}
+                                        overview={pelicula.overview}
+                                        type="movie" />
+                                ))}
+                            </div>}
+                    </section>
+                    <section>
+                        <h2 id="h2Main" className="nav-link"> Series Favoritas</h2>
+                        {this.state.dataSeries.length === 0 ? <p className="PFavoritos" id="copy">No tenés series favoritas</p> :
+                            <div className="divEnCartel">
+                                {this.state.dataSeries.map((pelicula, idx) => (
+                                    <CardSeries key={idx + 1}
+                                        img={pelicula.poster_path}
+                                        title={pelicula.name}
+                                        id={pelicula.id}
+                                        overview={pelicula.overview}
+                                        type="tv" />
+                                ))}
+                            </div>}
+                    </section>
+                </main>
             </div>
         );
     }
